1109d6548d0e3e74130ef8b7e6547f769a046923
KimJaewon3 <poilkj1po@gmail.com>
2022-06-20 19:24:27 +0900

    fix: 레이드 보상 모달 css 수정

--- client/src/components/rewardInfoModal.tsx
+++ client/src/components/rewardInfoModal.tsx
@@ -8,11 +8,22 @@ type RewardInfoModalProps = {
 const RewardInfoModalDiv = styled.div`
   position: absolute;
   top: 1.5em;
-  left: -636px;
+  left: -634px;
   border: 2px solid black;
   border-radius: 10px;
-  padding: 1em;
+  padding: 15px 10px 15px 15px;
   background-color: #429497;
+  max-height: 800px;
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 5px;
+  }
+  ::-webkit-scrollbar-thumb{
+    height: 30%;
+    background-color: #ffffff;
+    border-radius: 10px;    
+  }
   img {
     width: 600px;
   }
