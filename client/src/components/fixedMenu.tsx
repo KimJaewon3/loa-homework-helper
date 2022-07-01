8db4c7a0ff270082bb27044bc9b542d4482819ce
KimJaewon3 <poilkj1po@gmail.com>
2022-07-02 01:57:35 +0900

    feat: 입찰 계산기 기능 추가 & 맨 아래로 이동 버튼 추가

--- client/src/components/fixedMenu.tsx
+++ client/src/components/fixedMenu.tsx
@@ -1,27 +1,35 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import MemoBoard from '../modals/memoBoard';
-import { AiOutlineArrowUp } from "react-icons/ai";
+import { AiOutlineArrowUp, AiOutlineArrowDown } from "react-icons/ai";
+import LootCalculator from '../modals/lootCalculator';
 
 const FixedMenuDiv = styled.div`
   position: fixed;
   top: 35%;
   right: 0%;
-  > div {
+  .fixed-menu {
     padding: 3px;
     background-color: white;
     border: 2px solid black;
   }
-  .fixed-menu-goto-up {
+  .fixed-menu-goto-btn {
     position: fixed;
-    bottom: 10%;
+    background-color: #ffe38f;
     right: 0;
     margin: 2em;
-    padding: 0.7em;
+    padding: 0.5em;
     height: 20px;
     width: 20px;
     text-align: center;
-    background-color: #ffe38f;
+    border: 2px solid black;
+    border-radius: 5px;
+  }
+  .goto-up {
+    bottom: 15%;
+  }
+  .goto-down {
+    bottom: 10%;
   }
 `;
 
@@ -39,14 +47,26 @@ export default function FixedMenu({
   contentsStatusRef
 }: FixedMenuProps) {
   const [isMemoBoardOpen, setIsMemoBoardOpen] = useState(false);
+  const [isLootCalculatorOpen, setIsLootCalculatorOpen] = useState(false);
 
   function handleMemoBoardBtnClick() {
     setIsMemoBoardOpen(!isMemoBoardOpen);
   }
 
+  function handleLootCalculatorBtnClick() {
+    setIsLootCalculatorOpen(!isLootCalculatorOpen);
+  }
+
   function gotoUp() {
     window.scroll({
       top: 0,
+      behavior: 'smooth',
+    });
+  }
+
+  function gotoDown() {
+    window.scroll({
+      top: document.body.scrollHeight,
       behavior: 'smooth',
     });
   }
@@ -63,15 +83,16 @@ export default function FixedMenu({
   
   return (
     <FixedMenuDiv>
-      {isMemoBoardOpen && <MemoBoard />}
-      <div onClick={()=>gotoTargetRef(monitoringRef)}>전체현황</div>
-      <div onClick={()=>gotoTargetRef(characterRef)}>캐릭터현황</div>
-      <div onClick={()=>gotoTargetRef(contentsStatusRef)}>컨텐츠현황</div>
-      <div onClick={()=>gotoTargetRef(accountBookRef)}>골드현황</div>
-      <div onClick={handleMemoBoardBtnClick}>메모장</div>
-      <div className='fixed-menu-goto-up' onClick={gotoUp}>
-        <AiOutlineArrowUp />
-      </div>
+      {isMemoBoardOpen && <MemoBoard/>}
+      {isLootCalculatorOpen && <LootCalculator handleLootCalculatorBtnClick={handleLootCalculatorBtnClick}/>}
+      <div className='fixed-menu' onClick={()=>gotoTargetRef(monitoringRef)}>전체현황</div>
+      <div className='fixed-menu' onClick={()=>gotoTargetRef(characterRef)}>캐릭터현황</div>
+      <div className='fixed-menu' onClick={()=>gotoTargetRef(contentsStatusRef)}>컨텐츠현황</div>
+      <div className='fixed-menu' onClick={()=>gotoTargetRef(accountBookRef)}>골드현황</div>
+      <div className='fixed-menu' onClick={handleLootCalculatorBtnClick}>입찰계산기</div>
+      <div className='fixed-menu' onClick={handleMemoBoardBtnClick}>메모장</div>
+      <div className='fixed-menu-goto-btn goto-up' onClick={gotoUp}><AiOutlineArrowUp /></div>
+      <div className='fixed-menu-goto-btn goto-down' onClick={gotoDown}><AiOutlineArrowDown /></div>
     </FixedMenuDiv>
   )
 }
